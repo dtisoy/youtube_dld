145955ffa9046b3105189893404964aae3bce3f0
dtisoyUbuntu <danieltisoy2001@gmail.com>
2022-07-08 19:10:40 -0500

    added a log file

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+
 from youtube_api import YouTubeDataAPI
 from pytube import YouTube
 import os
 from dotenv import load_dotenv
 from cambiar_nombre import Rename_music
+import logging
 
+logging.basicConfig(filename="musica.log", level=logging.INFO)
 
 load_dotenv()
 key = os.getenv('API_KEY')
@@ -23,18 +26,25 @@ def run():
         playlist.append(f"https://www.youtube.com/watch?v={id_video}")
 
     # descargar el audio
+    contador = 0
     for video in playlist:
+        video_info = YouTube(video)
         try:
-            video_info = YouTube(video)
             video_info.streams.filter(
                 only_audio=True).first().download(output_path=path)
             print(f'{video_info.title} descargado')
+           #logging.info(f'{video_info.title} descargado')
+            contador += 1
         except:
-            print('No se pudo descargar')
+            logging.error(f'{video_info.title} No descargado')
+
+    print("_________________________")
+    print(f"{contador} canciones descargadas")
+    print("_________________________")
 
 
 if __name__ == '__main__':
-
+    logging.info("---- iniciando descargas ----")
     run()
 
     r_music = Rename_music()
